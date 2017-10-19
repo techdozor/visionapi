f171beacbb408f70aba7b8ecdd4a1eedf30a8f5b
Sergey A. Razin <sergey.razin@us.sios.com>
2017-10-19 11:33:28 -0400

    Finalizing the recognition methods

--- server.js
+++ server.js
@@ -10,18 +10,7 @@ var fileToRecognize = "/uploads/image.jpg";
 
 var upload = multer({ dest: 'uploads/' });
 
-
-/*app.post('/upload-target',function(req,res){
-    console.log("upload-target " + req.filename );
-    fs.readFile(req.file, function (err, data) {
-        // ...
-        var newPath = __dirname + "/uploads/uploadedFileName";
-        fs.writeFile(newPath, data, function (err) {
-        res.redirect("back");
-        });
-    });
-});*/
-
+//POST operation from upload html to upload and recognize the image
 app.post( '/upload-target', upload.single('file'), function(req,res){ 
     console.log("upload file "+req.file.path + " " + req.file.filename);
     fs.readFile(req.file.path, function (err, data) {
@@ -34,25 +23,90 @@ app.post( '/upload-target', upload.single('file'), function(req,res){
         if (err) throw err;
         console.log("successfully deleted " + req.file.path);
     });
+
+    recognize(fileToRecognize, function(err, result) {
+        if(err) {
+            console.log("Error occured recognizing the image");
+        } else {
+            console.log("Recognition was successful");
+        }
+    });
     res.status( 200 ).send( req.file );
 });
 
-app.get('/',function(req,res){
-  console.log("Sending data");
-  res.sendFile(__dirname + '/index.html');
-  //__dirname : It will resolve to your project folder.
-});
 
+//Image recognition function
+function recognize(fileSubPath) {
+    console.log("Calling recognize with: " + fileSubPath);
+    var fileName = __dirname + fileSubPath;
+    console.log("Full path for recognition: " + fileName);
+    data1 = "";
+    console.log(data1 + "\n");
+
+    // Instantiates a client
+    const vision = Vision();
+
+    // Performs label detection on the local file
+    vision.labelDetection({ source: { filename: fileName } })
+        .then((results) => {
+            const labels = results[0].labelAnnotations;
+            console.log('Labels:');
+            labels.forEach((label) => data1 += JSON.stringify(label.description) + "\n");
+            //console.log(data);
+        })
+        .catch((err) => {
+            console.error('ERROR:', err);
+        });
+
+    vision.logoDetection({ source: { filename: fileName } })
+        .then((results) => {
+            const logos = results[0].logoAnnotations;
+            console.log('Logos:');
+            logos.forEach((logo) => data1 += JSON.stringify(logo.description) + "\n");
+        })
+        .catch((err) => {
+        console.error('ERROR:', err);
+        });
+
+    // Performs landmark detection on the local file
+    vision.landmarkDetection({ source: {filename: fileName} })
+        .then((results) => {
+            const landmarks = results[0].landmarkAnnotations;
+            console.log('Landmarks:');
+            landmarks.forEach((landmark) => data1 += JSON.stringify(landmark.description) + "\n");
+        })
+        .catch((err) => {
+            console.error('ERROR:', err);
+        });
+
+    // Performs text detection on the local file
+    vision.textDetection({ source: { filename: fileName } })
+        .then((results) => {
+            const detections = results[0].textAnnotations;
+            console.log('Text:');
+            detections.forEach((label) => data1 += JSON.stringify(text.description) + "\n");
+            //console.log("Recognition result: \n" + data);
+        })
+        .catch((err) => {
+        console.error('ERROR:', err);
+        });
+
+    return;
+}
+
+//Render upload.html
 app.get('/upload',function(req,res){
     console.log("Upload file"); 
     res.sendFile(__dirname + '/upload.html');
 });
 
+//Provide the results of recognition
 app.get('/result',function(req,res){
     console.log(data1);
     res.send(data1);
 });
 
+//Test regonizer api
 app.get('/recognize',function(req, res){
     console.log("Recognize");  
     //var data = "test";
@@ -109,10 +163,6 @@ app.get('/recognize',function(req, res){
         console.error('ERROR:', err);
         });
     res.sendFile(__dirname + '/done.html');
-});
-
-app.get('/sitemap',function(req,res){
-  res.sendFile('sitemap.html');
 });
 
 app.listen(3000);
